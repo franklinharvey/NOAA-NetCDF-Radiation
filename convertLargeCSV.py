f228b0377eeda99eb7ec93405fea5416c82f2a1b
Franklin Harvey <franklinadamsharvey@gmail.com>
2016-12-06 10:19:44 -0700

    add ignore and new LargeCSV script

--- convertLargeCSV.py
+++ convertLargeCSV.py
@@ -1,22 +1,43 @@
+from os.path import basename
+import os
 import netCDF4 as nc
+import time
 import sys
 import csv
+import pandas as pd
 import numpy as np
 
 def openFile(filesToProcess):
+    df1 = pd.DataFrame()
+    counter = 0
     for input in filesToProcess:
-        input_file = csv.reader(input)
-        row_count = sum(1 for row in input_file)
+        print "1st check"
+        base = os.path.splitext(basename(input))[0]
+        print "Working on %s" % base
+        with open(input, 'r') as input_file:
+            #row_count = sum(1 for row in input_file)
+            if counter == 0:
+                print "2check"
+                df1 = createDataFrame(input_file, counter)
+            else:
+                print "3check"
+                df2 = createDataFrame(input_file, counter)
+                df1 = pd.concat([df1,df2])
+                print "4check"
+        counter += 1
+        input_file.close()
+        print "5check"
+    df1.to_csv('large.csv')
 
 def createDataFrame(input_file, counter):
     checkTime = time.clock()
-    #print "Start DataFrame -- #%d" % counter
+    print "Start DataFrame -- #%d" % counter
     df1 = pd.read_csv(input_file,
-            na_values = '-999.00',
             sep = ",",
+            #nrows = 5000,
             index_col = ['Date'])
-    #print "End DataFrame -- #%d" % counter
-    #print "Ran for " + str(time.clock() - checkTime) + " Seconds"
+    print "End DataFrame -- #%d" % counter
+    print "Ran for " + str(time.clock() - checkTime) + " Seconds"
     return df1
 
 if __name__ == '__main__':
