51bd8a3191b4f66a0223deefd0fa8b3e718061f7
Franklin Harvey <franklinadamsharvey@gmail.com>
2016-12-29 12:37:41 -0700

    still testing

--- test/test.py
+++ test/test.py
@@ -4,25 +4,52 @@ class findWord:
     name = ""
     length = 0
     count = 0
+    lineNumber = 0
 
 def openFile(input):
+    wordList = []
+    instanceList = []
     with open(input, 'r') as input_file:
-        count = 0
-        content = ""
-        find = "ALT_RAD2"
 
+        #populate list of words in header
         for count, line in enumerate(input_file):
-            fw = findWord
-            if count < 1:
-                print line
+            if count < 4:
                 elements = line.split()
                 for element in elements:
-                    fw.name = element
-                    fw.length = len(element)
-                    for characterCount,character in enumerate(line):
-                        for j in range(0,wordLength):
-                            if character == list(find)[0]:
-                                print str(characterCount) + "FOUND"
+                    wordList.append(element)
+
+    print "DONE"
+
+    #for each word in the header, find its attributes
+    for word in wordList:
+        with open(input, 'r') as input_file:
+            for count,line in enumerate(input_file):
+                #print count,word
+                if count<4:
+                    elements = line.split()
+                    for element in elements:
+                        fw = findWord
+                        fw.name = element
+                        print element
+                        fw.length = len(element)
+                        fw.lineNumber = count
+                        counter = 0
+                        for characterCount,character in enumerate(line):
+                            if character == list(word)[counter]:
+                                #print str(characterCount) + str(character) + str(list(find)[counter])
+                                counter +=1
+                                if counter == 3:
+                                    fw.count = characterCount-2
+                                    #print str(fw.count) + "WOW"
+                                    instanceList.append(fw)
+                                    counter = 0
+        #print wordList
+        for instance in instanceList:
+            print instance.name
+            print instance.length
+            print instance.count
+            print instance.lineNumber
+
 
 if __name__ == '__main__':
     openFile("alt_2004_09.dat")
