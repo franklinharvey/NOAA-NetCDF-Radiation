95d3d3f5ed3ccf4fcc3ab6e0c273dd2af6f4ce6b
Franklin Harvey <franklinadamsharvey@gmail.com>
2016-12-30 15:25:51 -0700

    redo file manager

--- convertDATtoCSVradiation.py
+++ convertDATtoCSVradiation.py
@@ -4,28 +4,38 @@ import os
 import csv
 import datParse
 
-def fileConvert(filesToProcess):
-	for input in filesToProcess:
-		instanceList = datParse.getInstanceList(input)
-		instanceList = datParse.sortInstanceList(instanceList)
-		instanceList = datParse.filterInstanceList(instanceList)
-		headers = datParse.getCSVHeaders(instanceList)
+def getHeaders(input):
+	"""Parses headers in .dat file and returns them comma seperated."""
+	instanceList = datParse.getInstanceList(input)
+	instanceList = datParse.sortInstanceList(instanceList)
+	instanceList = datParse.filterInstanceList(instanceList)
+	return datParse.getCSVHeaders(instanceList)
 
-		base = os.path.splitext(basename(input))[0]
-		baseFolder = base.split('_',1)[0] + "/csv/"
+def fileMGMT(filesToProcess):
+	if len(filesToProcess)>1:
+		for input in filesToProcess:
+			fileConvert(input)
+	else:
+		fileConvert(filesToProcess[0])
 
-		print "Processing %s" % base
-		out_name = ("../baselineRad/" + baseFolder + base + ".csv")
+def fileConvert(input):
+	"""Converts .dat to .csv file"""
+	base = os.path.splitext(basename(input))[0]
+	baseFolder = base.split('_',1)[0] + "/csv/"
+	headers = getHeaders(input)
 
-		with open(input, 'r') as input_file:
-			with open(out_name, 'w') as output_file:
-				output_file.write(headers + "\n")
-				for count, line in enumerate(input_file):
-					if count < 4:
-						pass
-					else:
-						outLine = ",".join(line.split())
-						output_file.write(outLine + '\n')
+	print "Processing %s" % base
+	out_name = ("../baselineRad/" + baseFolder + base + ".csv")
+
+	with open(input, 'r') as input_file:
+		with open(out_name, 'w') as output_file:
+			output_file.write(headers + "\n")
+			for count, line in enumerate(input_file):
+				if count < 4:
+					pass
+				else:
+					outLine = ",".join(line.split())
+					output_file.write(outLine + '\n')
 
 if __name__ == '__main__':
-	fileConvert(sys.argv[1:])
+	fileMGMT(sys.argv[1:])
