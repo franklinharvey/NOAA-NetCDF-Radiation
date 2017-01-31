52f34a99718af958b021f3e4516a5f3cf3c4ccdc
Franklin Harvey <franklinadamsharvey@gmail.com>
2017-01-31 14:53:16 -0700

    add source

--- add_attribute.py
+++ add_attribute.py
@@ -8,6 +8,8 @@ def file_mgmt(filesToProcess):
         foo=nc.Dataset(input,'r+')
         foo.Conventions='CF-1.6'
         foo.title = 'NOAA/ESRL/GMD/GRAD Radiation Archive - %s' % (get_testsite(input))
+        foo.source = 'https://github.com/franklinharvey/NOAA-NetCDF-Radiation'
+        foo.comment="Converted from .dat file in the radiation group's FTP server"
 
 def get_filename(input):
 	"""Returns the name of the file without the file extension"""
@@ -17,11 +19,6 @@ def get_testsite(input):
 	"""Returns the name of the testing location"""
 	base = get_filename(input)
 	return base.split('_',1)[0]
-
-
-
-
-
 
 if __name__ == '__main__':
 	df = file_mgmt(sys.argv[1:])
